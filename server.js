7adf5fd362a197fbdc660e8419db05d94f4ce96a
Hilda <hildamwng868@gmail.com>
2015-09-24 15:52:10 +0300

    created routers to handle getting all the bears and creating a bear

--- server.js
+++ server.js
@@ -44,8 +44,27 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
+//registering Routes
+app.use('/api', router);
 
+// more routes for our API will happen here
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+        
+    });
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
