d67bbf04480f16f17d707d1010c817858d93bb9d
Hilda <hildamwng868@gmail.com>
2015-09-24 15:41:52 +0300

    added DB URL, added new files--> app/models/bear.js

--- server.js
+++ server.js
@@ -23,8 +23,32 @@ var router = express.Router();              // get an instance of the express Ro
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+// connect to our db
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/rest'); 
+var Bear      =require('./app/models/bear');
+// more routes for our API will happen here
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
 // more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
